4a4d70b925d93fa0a61e4fd902b11ac38924aeb9
Meriiiiieee <meri.hakobyan21@edu.ysu.am>
2025-08-25 14:19:09 +0400

    4

---
+++ strcmp.c
@@ -0,0 +1,9 @@
+#include "strcmp.h"
+
+int mystrcmp(const char *str1, const char *str2) {
+    while ((*str1 != '\0') && (*str1 == *str2)) {
+        str1++;
+        str2++;
+    }
+    return *str1 - *str2;
+}
