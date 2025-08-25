d9c59961a961f36ab9bc0cf4d10c63c823a097ed
Meriiiiieee <meri.hakobyan21@edu.ysu.am>
2025-08-25 14:04:30 +0400

    2

---
+++ strcpy.c
@@ -0,0 +1,13 @@
+
+#include "strcpy.h"
+
+char *my_strcpy(char *dest, const char *src) {
+    char *ptr = dest;
+    while (*src != '\0') {
+        *dest = *src;
+        dest++;
+        src++;
+    }
+    *dest = '\0'; 
+    return ptr;
+}
